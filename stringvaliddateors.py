1b04041d96e2fd9d55c7ad0b96399c371229f5f5
Mahesh Alladi <alladimaheshbabu333@gmail.com>
2019-11-08 16:12:47 +0530

    Create stringvaliddateors.py

---
+++ stringvaliddateors.py
@@ -0,0 +1,33 @@
+if __name__ == '__main__':
+    s = input()
+for i in range(0,len(s)):
+    if s[i].isalnum():
+        print("True")
+        break
+else:
+    print("False")
+for i in range(0,len(s)):
+    if s[i].isalpha():
+        print("True")
+        break
+else:
+    print("False")
+for i in range(0,len(s)):
+    if s[i].isdigit():
+        print("True")
+        break
+else:
+    print("False")
+for i in range(0,len(s)):
+    if s[i].islower():
+        print("True")
+        break
+else:
+    print("False")
+for i in range(0,len(s)):
+    if s[i].isupper():
+        print("True")
+        break
+else:
+    print("False")
+
